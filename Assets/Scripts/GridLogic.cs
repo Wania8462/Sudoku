c696fe1a3eb245ddeadf94e04ecc92b524a676fb
Wania8462 <65669738+Wania8462@users.noreply.github.com>
2020-05-20 20:10:29 +0300

    Initial commit

---
+++ Assets/Scripts/GridLogic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridLogic : MonoBehaviour
+{
+    [SerializeField] GameObject Grid; // объект
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int p = -4; p < 5; p++)
+        
+        for (int P = 0; P < 9; P++)
+        
+        Instantiate(Grid, new Vector3(p, P, 0), new Quaternion()); // (x, y, z) где будут стоять поле для цифр
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
